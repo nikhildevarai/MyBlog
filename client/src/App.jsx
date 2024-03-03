8fe36f191848f98f17b64f88dfb5e26d7c48a2d8
nikhildevarai <Nikhil.Devarakonda@lntinfotech.com>
2024-03-03 23:35:26 +0530

    Created navbar and added routing to it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Projects from "./pages/Projects";
+import MyTours from "./pages/MyTours";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/mytours" element={<MyTours />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/MyTours.jsx
@@ -0,0 +1,7 @@
+export default function MyTours() {
+  return (
+    <div>
+      <h1>My personal Life</h1>
+    </div>
+  );
+}
