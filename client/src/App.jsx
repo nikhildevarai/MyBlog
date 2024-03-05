5301416aa63e4cfbcbf0f7a79f6d99790b1a90fe
nikhildevarai <Nikhil.Devarakonda@lntinfotech.com>
2024-03-05 14:31:21 +0530

    created Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import MyTours from "./pages/MyTours";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/mytours" element={<MyTours />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
